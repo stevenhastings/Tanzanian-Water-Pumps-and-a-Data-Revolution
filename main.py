8f92faab49b812d818e4a0042f81ca00aa696372
Steven Hastings <steven.hastings@gmail.com>
2022-07-26 08:34:53 -0400

    added live map data

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-train = pd.merge(pd.read_csv('Tanzanian-Water-Pumps-and-a-Data-Revolution/train_features.csv'),
+train = pd.merge(pd.read_csv('C:\\Users\\steve\\WATERPUMPS\\Tanzanian-Water-Pumps-and-a-Data-Revolution\\train_features.csv'),
                 pd.read_csv('Tanzanian-Water-Pumps-and-a-Data-Revolution/train_labels.csv'))
 
 test = pd.read_csv('Tanzanian-Water-Pumps-and-a-Data-Revolution/test_features.csv')
@@ -11,4 +11,8 @@ sample_submission = pd.read_csv('Tanzanian-Water-Pumps-and-a-Data-Revolution/Sub
 
 # Splitting the Data
 
-train, val = train_test_split(train, train_size)
+train, val = train_test_split(train, train_size=0.80, test_size=0.20,
+                                stratify=train['status_group'], random_state=42)
+
+print(f"train shape: {train.shape}, \n val shape: {val.shape}, \n test shape: {test.shape}")
+
